268cb3176a488b68ee4a150342d59e9a2d74e20b
Julian R <56316686+julianmrodri@users.noreply.github.com>
2020-04-04 16:04:03 -0300

    Adding components

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,21 @@ import React from 'react';
 import './App.css';
 
 import { useWeb3 } from '@openzeppelin/network/react';
+
+import Web3Data from './components/Web3Data.js';
+
 const infuraProjectId = '50f18860239f4afeaa418afb76717f3c';
 
 function App() {
   const web3Context = useWeb3(`wss://mainnet.infura.io/ws/v3/${infuraProjectId}`);
-  const { networkId, networkName, providerName } = web3Context;
-
-  
-  return (
- <div className="App">
-  <div>
-	<h1>Infura/MetaMask/OpenZeppelin Dapp</h1>
-		<div>
-    	Network: {networkId ? `${networkId} – ${networkName}` : 'No connection'}
-		</div>
-		<div>
-		Provider: {providerName}
-		</div>
+return (
+<div className="App">
+	<div>
+	<h1>Infura React Dapp with Components!</h1>
+	<Web3Data title="Web3 Data" web3Context={web3Context} />
 	</div>
 </div>
-  );
+);
 }
 
 export default App;
