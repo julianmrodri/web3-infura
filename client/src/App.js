a11b2c1183458ed1322e1881d102d1d7b9724eaa
Julian R <56316686+julianmrodri@users.noreply.github.com>
2020-04-04 15:43:01 -0300

    Updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { useWeb3 } from '@openzeppelin/network/react';
+const infuraProjectId = '50f18860239f4afeaa418afb76717f3c';
+
 function App() {
+  const web3Context = useWeb3(`wss://mainnet.infura.io/ws/v3/${infuraProjectId}`);
+  const { networkId, networkName, providerName } = web3Context;
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <div className="App">
+  <div>
+	<h1>Infura/MetaMask/OpenZeppelin Dapp</h1>
+		<div>
+    	Network: {networkId ? `${networkId} – ${networkName}` : 'No connection'}
+		</div>
+		<div>
+		Provider: {providerName}
+		</div>
+	</div>
+</div>
   );
 }
 
 export default App;
+
